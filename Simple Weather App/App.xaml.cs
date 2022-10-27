b8526bb65b84d0bc7729072cc72ac68526fea6a6
Haintrain <jelly.han01@gmail.com>
2022-10-28 12:22:04 +1300

    Initial commit

---
+++ Simple Weather App/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Simple_Weather_App
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
